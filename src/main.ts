2cadf11758263406e0557f86cc3254be79e05838
Maxim Pashentsev <maks.p.48@gmail.com>
2023-08-07 23:04:59 +0300

    Cors: True

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(MainModule);
+
+  const app = await NestFactory.create(MainModule, { cors: true });
 
   const config = new DocumentBuilder()
-  .setTitle('CafeSchedule')
-  .setVersion('1.0')
-  .build();
+    .setTitle('CafeSchedule')
+    .setVersion('1.0')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
